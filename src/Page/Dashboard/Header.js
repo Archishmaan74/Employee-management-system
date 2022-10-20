1c0b7ef05e94faddee3bf69d73f6f9e2d8a9ae0a
Archishmaan <archishmaan.upa4@gmail.com>
2022-10-20 12:18:39 +0530

    Changed header and button design

--- src/Page/Dashboard/Header.js
+++ src/Page/Dashboard/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Header({ setIsAdding }) {
     return (
         <header>
-            <h1>Employee Management System</h1>
+            <div style={{ marginLeft: "300px"}}>
+                <h1>Employee Management System</h1>
+            </div>
             <div style={{ marginTop: '30px', marginBottom: '18px', marginLeft: '500px' }}>
                 <button onClick={() => setIsAdding(true)} className='round-button'>Add Button</button>
             </div>
